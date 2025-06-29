709a5d2e59c1b4a928ab3596c3bb42baafae0d9d
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-06-29 21:04:11 +0600

    .

---
+++ lib/ui/screens/sing_in_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SingInScreen extends StatefulWidget {
+  const SingInScreen({super.key});
+
+  @override
+  State<SingInScreen> createState() => _SingInScreenState();
+}
+
+class _SingInScreenState extends State<SingInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome to dark Place'),
+      ),
+    );
+  }
+}
